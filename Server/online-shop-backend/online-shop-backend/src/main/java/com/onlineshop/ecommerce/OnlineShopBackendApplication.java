a2ca8095988ad1fa2253e175fd59b57abf4f58ac
ErikV121 <ErikVasquez121@gmail.com>
2024-05-10 18:02:12 -0400

    Added Spring boot project

---
+++ Server/online-shop-backend/online-shop-backend/src/main/java/com/onlineshop/ecommerce/OnlineShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.onlineshop.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopBackendApplication.class, args);
+	}
+
+}
