ac70bc28aad303940ae9324bedece4cf3a9bdd6e
surgosan <sergy200118@gmail.com>
2024-05-11 03:22:08 -0400

    Output Controller

---
+++ client/vue.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+// Comment out to serve production version
+module.exports = {
+    outputDir: path.resolve(__dirname, '../template'),
+    assetsDir: '../static',
+}
